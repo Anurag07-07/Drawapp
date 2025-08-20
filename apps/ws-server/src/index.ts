cf657b99fb37fb5a2c2c6f3abe6da1af2678ef8a
Anurag Raj <adityanv8@gmail.com>
2025-08-21 02:40:16 +0530

    complete room enter and exit logic in ws-server

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,40 +1,87 @@
-import { WebSocketServer } from "ws";
-import jwt from 'jsonwebtoken'
-import dotenv from "dotenv"
-dotenv.config({ path: "../../.env" }) 
+import { WebSocketServer, WebSocket } from "ws";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({ path: "../../.env" });
 
-interface JwtPayload{
-  id:string
+interface JwtPayload {
+  id: string;
 }
 
-const wss = new WebSocketServer({port:8080})
-
-function checkUser(token:string):string | null {
-  const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload
-
-  if (!decoded || !decoded.id) {
-    return null
-  }
-
-  return decoded.id
+interface User {
+  socket: WebSocket;
+  room: string[];
+  userId: string;
 }
 
-wss.on('connection',(socket,request)=>{
-  const url = request.url
+const wss = new WebSocketServer({ port: 8080 });
+
+const users: User[] = [];
+
+wss.on("connection", (socket, request) => {
+  const url = request.url;
   if (!url) {
-    return
-  }
-  //Get the url and form array
-  const urlParams = new URLSearchParams(url.split('?')[1])
-  const tokenvalue = urlParams.get('token') || ""  as string
-  const UserValidation = checkUser(tokenvalue)
-  if (!UserValidation) {
-    wss.close()
+    socket.close(1008, "Missing URL");
+    return;
   }
 
-  socket.on('message',(e)=>{
-    if (e.toString()==='Hey') {
-      socket.send("Hello There")
+  try {
+    //Get the url and form array
+    const urlParams = new URLSearchParams(url.split("?")[1]);
+    const tokenvalue = urlParams.get("token") || "";
+    console.log(tokenvalue);
+
+    const decoded = jwt.verify(
+      tokenvalue,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decoded || !decoded.id) {
+      socket.close(1008, "Invalid token");
+      return;
     }
-  })
-})
+
+
+    // Store user
+    users.push({
+      userId: decoded.id,
+      socket: socket,
+      room: [],
+    });
+
+    socket.on("message", (data) => {
+      try {
+        const parsedData = JSON.parse(data.toString() as unknown as string);
+        //If the User Send type === 'join_room' we have to add the user in the room
+        if (parsedData.type === 'join_room') {
+          //Find the user with the socket Id 
+          const user = users.find((u)=>u.socket === socket)
+          //Now as the user is found we add that user in the room
+          console.log(parsedData.room);
+          user?.room.push(parsedData.room) 
+          socket.send("You joined the Room")
+        }
+
+        //If the user come out from the we have to remove that user from the room
+        if (parsedData.type === 'exit_room') {
+          //Find the user 
+          const user = users.find((u)=>u.socket === socket)
+
+          //Now we have to filter the room array and remove that particular room
+          if (user) {
+            user.room = user.room.filter((r)=>r!== parsedData.room)
+            socket.send('Exit from the room')
+          }
+        }
+      } catch (err) {
+        console.error("Invalid JSON message:", err);
+        socket.send("Invalid message format");
+      }
+    });
+
+    console.log(users);
+
+  } catch (err) {
+    console.error("JWT Error:", err);
+    socket.close(1008, "Unauthorized");
+  }
+});
