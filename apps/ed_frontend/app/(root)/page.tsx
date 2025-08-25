cb329db8a5b863e70cf6415597cc1c05b9c526de
Anurag Raj <adityanv8@gmail.com>
2025-08-26 02:27:45 +0530

    Create New Next App For Canvas Logic

---
+++ apps/ed_frontend/app/(root)/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  return <div>
+    ExcilDraw Landing Page
+  </div>
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Main() {
   return <div>
-      hello There
+    
   </div>
 }
