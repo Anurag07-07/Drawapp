3789227712dcc46443d7b00fe113bfb3dc0322dd
Anurag Raj <adityanv8@gmail.com>
2025-09-04 00:48:45 +0530

    complete_toggle_navigation_logic

--- apps/web/app/Components/Navbar.tsx
+++ apps/web/app/Components/Navbar.tsx
@@ -1,12 +1,33 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
-import Logo from '../images/logo.svg'
-
+import React, { useState } from 'react'
+import logo from '../images/logo.svg'
+import { IoMenuOutline } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
 const Navbar = () => {
+
+  const [toogle,setToogle] = useState<boolean>(false)
+
+  function ChangeToogle() {
+    setToogle((toggle)=>!toggle)
+  }
+
   return (
-    <div className=' bg-white w-full h-screen'>
-      <Image alt='logo' src={Logo}></Image>
+    <>
+    <div className=' flex justify-between py-5 px-2'>
+      <div>
+        <Image width={250} src={logo} alt='logo'></Image>  
+      </div>
+      <div onClick={ChangeToogle} className=' flex justify-center items-center border rounded-full px-2'>{<IoMenuOutline color='blue' size={30}></IoMenuOutline>}Menu</div>
     </div>
+    <div>
+      {
+        toogle && <div className=' bg-white h-screen fixed top-0 z-50 w-full'>
+          <div className=' absolute right-10 top-10'>{<RxCross1 size={30} onClick={ChangeToogle} />}</div>
+        </div>
+      }
+    </div>
+    </>
   )
 }
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./Components/Navbar";
 
 export default function Page() {
-  return <div className=" bg-white-100 w-full h-screen">
+  return <div>
     <Navbar></Navbar>
   </div>
 }
